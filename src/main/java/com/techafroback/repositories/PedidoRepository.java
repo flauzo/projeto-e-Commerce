0b4058879fd5285f4a42ec45ea2146a815481a92
Cruz, Flausimar <88656939+flauzo@users.noreply.github.com>
2024-11-15 03:49:10 -0300

    Update PedidoRepository.java

--- src/main/java/com/techafroback/repositories/PedidoRepository.java
+++ src/main/java/com/techafroback/repositories/PedidoRepository.java
@@ -5,6 +5,6 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.techafroback.model.Pedido;
 
-
 public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
 }
+
