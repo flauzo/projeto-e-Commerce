aa3b72a8fc9666438723ca1af5d94b7cb5aa34cd
Cruz, Flausimar <88656939+flauzo@users.noreply.github.com>
2024-11-15 04:01:00 -0300

    Update UsuarioController.java

--- src/main/java/com/techafroback/controllers/UsuarioController.java
+++ src/main/java/com/techafroback/controllers/UsuarioController.java
@@ -3,6 +3,7 @@ package com.techafroback.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -44,5 +45,5 @@ public class UsuarioController {
 	public void delete(@PathVariable Long id){
 		repository.deleteById(id);		
 	}
-
 }
+
