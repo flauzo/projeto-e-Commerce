4bca1dbe379bbda972f8c9b3a0b45bab4ee11d2a
Cruz, Flausimar <88656939+flauzo@users.noreply.github.com>
2024-11-15 04:04:44 -0300

    Update CategoriaController.java

--- src/main/java/com/techafroback/controllers/CategoriaController.java
+++ src/main/java/com/techafroback/controllers/CategoriaController.java
@@ -3,6 +3,7 @@ package com.techafroback.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -49,5 +50,5 @@ public class CategoriaController {
 	public void delete(@PathVariable Long id){
 		repository.deleteById(id);		
 	}
-
 }
+
