2d029165bbd7af55fb50a5fa45e1ef4480a7054e
Cruz, Flausimar <88656939+flauzo@users.noreply.github.com>
2024-11-15 03:52:38 -0300

    Update Usuario.java

--- src/main/java/com/techafroback/model/Usuario.java
+++ src/main/java/com/techafroback/model/Usuario.java
@@ -1,12 +1,12 @@
 package com.techafroback.model;
 
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -73,5 +73,5 @@ public class Usuario {
   public String getSenha() {
     return senha;
   }
-
 }
+
