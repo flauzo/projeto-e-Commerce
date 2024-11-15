80fbce1363fd8607cd484116ecb0a2f9adc61cb3
Cruz, Flausimar <88656939+flauzo@users.noreply.github.com>
2024-11-15 03:48:26 -0300

    Update UsuarioRepository.java

--- src/main/java/com/techafroback/repositories/UsuarioRepository.java
+++ src/main/java/com/techafroback/repositories/UsuarioRepository.java
@@ -10,3 +10,4 @@ import com.techafroback.model.Usuario;
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
   public Optional<Usuario> findByUsuario(@Param ("usuario")String usuario);
 }
+
